575c38be8a499e1f6adef33d45ce8ba1c91e2a20
Илья <36798906+N713@users.noreply.github.com>
2022-12-08 18:23:19 +0300

    Add files via upload

---
+++ lucky_test/js/script.js
@@ -0,0 +1,125 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./source/js/script.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./source/js/components/burger.js":
+/*!****************************************!*\
+  !*** ./source/js/components/burger.js ***!
+  \****************************************/
+/*! exports provided: setBurger */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"setBurger\", function() { return setBurger; });\nvar burger = document.body.querySelector('.burger');\nvar menu = document.body.querySelector('.header__nav');\nvar setBurger = function setBurger() {\n  burger.addEventListener('click', function (evt) {\n    evt.preventDefault();\n    burger.classList.toggle('opened');\n    menu.classList.toggle('opened');\n  });\n};\n\n\n//# sourceURL=webpack:///./source/js/components/burger.js?");
+
+/***/ }),
+
+/***/ "./source/js/components/description.js":
+/*!*********************************************!*\
+  !*** ./source/js/components/description.js ***!
+  \*********************************************/
+/*! exports provided: setDescription */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"setDescription\", function() { return setDescription; });\nvar setDescription = function setDescription() {\n  var URL = 'https://baconipsum.com/api/?type=lucky';\n  var xhr = new XMLHttpRequest();\n  xhr.responseType = 'json';\n  xhr.addEventListener('load', function () {\n    if (xhr.status === 200) {\n      document.body.querySelector('.info p:first-of-type').textContent = xhr.response[length];\n    } else {\n      console.log('Error!');\n    }\n  });\n  xhr.open('GET', URL);\n  xhr.send();\n};\n\n\n//# sourceURL=webpack:///./source/js/components/description.js?");
+
+/***/ }),
+
+/***/ "./source/js/script.js":
+/*!*****************************!*\
+  !*** ./source/js/script.js ***!
+  \*****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_burger__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/burger */ \"./source/js/components/burger.js\");\n/* harmony import */ var _components_description__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/description */ \"./source/js/components/description.js\");\n\n\nObject(_components_burger__WEBPACK_IMPORTED_MODULE_0__[\"setBurger\"])();\nObject(_components_description__WEBPACK_IMPORTED_MODULE_1__[\"setDescription\"])();\n\n//# sourceURL=webpack:///./source/js/script.js?");
+
+/***/ })
+
+/******/ });
