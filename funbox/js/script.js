af8749c6ac2fd20cea486e8e8809c059a0d3f25a
Илья <36798906+N713@users.noreply.github.com>
2023-01-18 16:41:24 +0300

    Add files via upload

---
+++ funbox/js/script.js
@@ -0,0 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./source/js/script.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./source/js/components/packs.js":
+/*!***************************************!*\
+  !*** ./source/js/components/packs.js ***!
+  \***************************************/
+/*! exports provided: setPacks */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"setPacks\", function() { return setPacks; });\nvar ENTER_KEYCODE = 13;\nvar SPACE_KEYCODE = 32;\nvar packs = document.body.querySelectorAll('.pack');\nvar selectPack = function selectPack(item) {\n  item.classList.toggle('pack--selected');\n  if (item.classList.contains('pack--selected')) {\n    item.classList.add('pack--no-hover');\n  }\n};\nvar setPacks = function setPacks() {\n  packs.forEach(function (pack) {\n    pack.querySelector('.pack__main').addEventListener('click', function () {\n      selectPack(pack);\n    });\n    pack.querySelector('.pack__text a').addEventListener('click', function (evt) {\n      evt.preventDefault();\n      selectPack(pack);\n    });\n    pack.addEventListener('mouseenter', function () {\n      pack.classList.remove('pack--no-hover');\n    });\n    pack.addEventListener('keydown', function (evt) {\n      if (evt.keyCode === SPACE_KEYCODE || evt.keyCode === ENTER_KEYCODE) {\n        pack.classList.toggle('pack--selected');\n      }\n    });\n  });\n};\n\n\n//# sourceURL=webpack:///./source/js/components/packs.js?");
+
+/***/ }),
+
+/***/ "./source/js/script.js":
+/*!*****************************!*\
+  !*** ./source/js/script.js ***!
+  \*****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_packs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/packs */ \"./source/js/components/packs.js\");\n\nif (document.body) {\n  Object(_components_packs__WEBPACK_IMPORTED_MODULE_0__[\"setPacks\"])();\n}\n\n//# sourceURL=webpack:///./source/js/script.js?");
+
+/***/ })
+
+/******/ });
